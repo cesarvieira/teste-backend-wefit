08bc741fb34d8b09fd6fe96660708d640fa8dc49
Cesar Vieira <cesar@kaisari.com.br>
2024-10-14 11:02:12 -0300

    Swagger docs

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,23 @@ import 'dotenv/config';
 import express from "express";
 import bodyParser from "body-parser";
 import personRoutes from "./routes/person.routes";
+import swaggerUi from "swagger-ui-express";
+import fs from 'fs';
+import path from 'path';
 
+const env = process.env.NODE_ENV || "development";
+const port = process.env.PORT || 4568;
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const port = process.env.PORT || 4568;
+if (env === "development") {
+  const swaggerFilePath = path.join(__dirname, '../swagger.json');
+  const swaggerDocument = JSON.parse(fs.readFileSync(swaggerFilePath, 'utf-8'));
+
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+}
 
 app.use("/person", personRoutes);
 
@@ -18,4 +28,7 @@ app.get("/ping", (_req: express.Request, res: express.Response) => {
 
 app.listen(port, () => {
   console.log(`Escutando na porta ${port}`);
+  if (env === "development") {
+    console.log(`Documentação disponível em http://localhost:${port}/api-docs`);
+  }
 });
